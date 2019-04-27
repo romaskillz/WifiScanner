cdf51c37414b02e4b264f58ed343102767b43c6c
Roman Telukhin <romaskillz@gmail.com>
2019-04-27 12:18:55 +0300

    Added methods in scan fragment

--- app/src/main/java/com/example/roman/wifiscanner/fragment_scan/ScanListFragment.kt
+++ app/src/main/java/com/example/roman/wifiscanner/fragment_scan/ScanListFragment.kt
@@ -1,10 +1,27 @@
 package com.example.roman.wifiscanner.fragment_scan
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.roman.wifiscanner.R
 import com.hannesdorfmann.mosby3.mvp.MvpFragment
 
-class ScanListFragment : MvpFragment<ScanListPresenter, IScanView>(), IScanView {
+class ScanListFragment : MvpFragment<IScanView, ScanListPresenter>(), IScanView {
 
-    override fun createPresenter(): IScanView {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_scan_list, container, false)
+    }
+
+    override fun createPresenter(): ScanListPresenter {
+        TODO("not implemented: need to create presenter")
+    }
+
+    fun startScan() {
+        //TODO("not implemented") presenter.startScan()
+    }
+
+    fun onItemClick() {
+        //TODO("not implemented") need to handle onItem click
     }
 }
--- app/src/main/java/com/example/roman/wifiscanner/fragment_scan/ScanListPresenter.kt
+++ app/src/main/java/com/example/roman/wifiscanner/fragment_scan/ScanListPresenter.kt
@@ -3,9 +3,9 @@ package com.example.roman.wifiscanner.fragment_scan
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter
 
 class ScanListPresenter : MvpBasePresenter<IScanView>() {
-    override fun attachView(view: IScanView) {
-        super.attachView(view)
-        ifViewAttached { view ->
-        }
+
+
+    fun startScan(){
+
     }
 }
